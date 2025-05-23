d7b15f7aa900461a9edfbd01c87a7cb11edf180e
Yeider15 <ye.catari@duocuc.cl>
2025-05-22 21:03:33 -0400

    subiendo proyecto a git

---
+++ src/main/java/com/ymy/cl/perfulandiafinal/PerfulandiafinalApplication.java
@@ -0,0 +1,13 @@
+package com.ymy.cl.perfulandiafinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiafinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiafinalApplication.class, args);
+	}
+
+}
