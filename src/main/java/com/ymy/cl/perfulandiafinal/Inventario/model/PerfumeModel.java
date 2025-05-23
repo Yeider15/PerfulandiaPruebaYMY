725a4919ecae77be18b07bd3be1e0f72e198ab8e
irmar <ya.rugel@duocuc.cl>
2025-05-22 21:54:04 -0400

    creacion de la funcionalidad

--- src/main/java/com/ymy/cl/perfulandiafinal/Inventario/model/PerfumeModel.java
+++ src/main/java/com/ymy/cl/perfulandiafinal/Inventario/model/PerfumeModel.java
@@ -1,4 +1,42 @@
 package com.ymy.cl.perfulandiafinal.Inventario.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "perfume")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PerfumeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id ;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = true)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private BigDecimal precio;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(nullable = false)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Date fechaIngreso;
+
 }
--- src/main/java/com/ymy/cl/perfulandiafinal/Inventario/repository/PerfumeRepository.java
+++ src/main/java/com/ymy/cl/perfulandiafinal/Inventario/repository/PerfumeRepository.java
@@ -1,4 +1,10 @@
 package com.ymy.cl.perfulandiafinal.Inventario.repository;
+import com.ymy.cl.perfulandiafinal.Inventario.model.PerfumeModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class PerfumeRepository {
+@Repository
+public interface PerfumeRepository extends JpaRepository<PerfumeModel, Integer> {
+    // Buscar perfume por su nombre
+    PerfumeModel findByNombre(String nombre);
 }
